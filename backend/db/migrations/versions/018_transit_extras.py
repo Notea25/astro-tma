5dcbf6005b33601cf7068cae5e699c5ff9f160a1
samandr77 <asamosuk62@gmail.com>
2026-06-03 20:22:48 +0300

    fix(migrations): silence transit import ordering

--- backend/db/migrations/versions/018_transit_extras.py
+++ backend/db/migrations/versions/018_transit_extras.py
@@ -18,7 +18,7 @@ Revision ID: 018_transit_extras
 Create Date: 2026-06-03
 """
 
-import sqlalchemy as sa
+import sqlalchemy as sa  # noqa: I001
 from alembic import op
 
 revision = "018_transit_extras"
